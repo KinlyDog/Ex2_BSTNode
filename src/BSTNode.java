f377b012ee6fbd65ab532d0d7bc127615e6cca0c
KinlyDog <kinlydog@gmail.com>
2023-04-03 21:39:28 +1000

    Update project

--- src/BSTNode.java
+++ src/BSTNode.java
@@ -39,11 +39,9 @@ class BST<T> {
     public BSTFind<T> FindNodeByKey(int key) {
         BSTFind<T> BSTF = new BSTFind<>();
 
-        if (Root == null) {
-            return BSTF;
+        if (Root != null) {
+            FindNodeByKeyRec(Root, key, BSTF);
         }
-
-        FindNodeByKeyRec(Root, key, BSTF);
 
         return BSTF;
     }
@@ -73,6 +71,11 @@ class BST<T> {
     }
 
     public boolean AddKeyValue(int key, T val) {
+        if (Root == null) {
+            Root = new BSTNode<>(key, val, null);
+            return true;
+        }
+
         BSTFind<T> BSTFNode = FindNodeByKey(key);
 
         if (BSTFNode.NodeHasKey) {
@@ -93,6 +96,10 @@ class BST<T> {
     }
 
     public BSTNode<T> FinMinMax(BSTNode<T> fromNode, boolean findMax) {
+        if (fromNode == null) {
+            return null;
+        }
+
         if (findMax) {
             return findMax(fromNode);
         }
@@ -118,29 +125,36 @@ class BST<T> {
 
     public boolean DeleteNodeByKey(int key) {
         BSTFind<T> BSTF = FindNodeByKey(key);
-        if (!BSTF.NodeHasKey) {
+        if (Root == null || !BSTF.NodeHasKey) {
             return false;
         }
 
-        boolean parentRightChild = BSTF.Node.Parent.RightChild == BSTF.Node;
         boolean leftNull = BSTF.Node.LeftChild == null;
         boolean rightNull = BSTF.Node.RightChild == null;
 
-        if (leftNull && rightNull) {
-            DeleteFirst(BSTF, parentRightChild);
-        } else if (leftNull || rightNull) {
-            DeleteSecond(BSTF, parentRightChild, leftNull, rightNull);
-        } else if (!leftNull && !rightNull) {
-            DeleteThird(BSTF, parentRightChild);
+        // написать код удаления узла Root
+        if (BSTF.Node == Root && leftNull && rightNull) {
+            Root = null;
+            return true;
         }
 
-        DeleteThird(BSTF, parentRightChild);
+        if (leftNull && rightNull) {
+            DeleteFirst(BSTF);
+        } else if (leftNull || rightNull) {
+            DeleteSecond(BSTF, leftNull, rightNull);
+        } else {
+            DeleteThird(BSTF);
+        }
+
+//        DeleteThird(BSTF, parentRightChild);
         count--;
 
         return true;
     }
 
-    public void DeleteFirst(BSTFind<T> BSTF, boolean parentRightChild) {
+    public void DeleteFirst(BSTFind<T> BSTF) {
+        boolean parentRightChild = BSTF.Node.Parent.RightChild == BSTF.Node;
+
         if (parentRightChild) {
             BSTF.Node.Parent.RightChild = null;
         } else {
@@ -150,7 +164,23 @@ class BST<T> {
         BSTF.Node.Parent = null;
     }
 
-    public void DeleteSecond(BSTFind<T> BSTF, boolean parentRightChild, boolean leftNull, boolean rightNull) {
+    public void DeleteSecond(BSTFind<T> BSTF, boolean leftNull, boolean rightNull) {
+//        if (BSTF.Node == Root && leftNull) {
+//            Root = BSTF.Node.RightChild;
+//            BSTF.Node.RightChild = null;
+//            Root.Parent = null;
+//            return;
+//        }
+//
+//        if (BSTF.Node == Root && rightNull) {
+//            Root = BSTF.Node.LeftChild;
+//            BSTF.Node.LeftChild = null;
+//            Root.Parent = null;
+//            return;
+//        }
+
+        boolean parentRightChild = BSTF.Node.Parent.RightChild == BSTF.Node;
+
         if (leftNull) {
             if (parentRightChild) {
                 BSTF.Node.Parent.RightChild = BSTF.Node.RightChild;
@@ -176,7 +206,9 @@ class BST<T> {
         BSTF.Node.Parent = null;
     }
 
-    public void DeleteThird(BSTFind<T> BSTF, boolean parentRightChild) {
+    public void DeleteThird(BSTFind<T> BSTF) {
+        boolean parentRightChild = BSTF.Node.Parent.RightChild == BSTF.Node;
+
         BSTNode<T> node = DeleteThirdRec(BSTF.Node.RightChild);
 
         if (parentRightChild) {
@@ -185,12 +217,32 @@ class BST<T> {
             BSTF.Node.Parent.LeftChild = node;
         }
 
-        node.Parent.LeftChild = null;
-        node.LeftChild = BSTF.Node.LeftChild;
-        node.RightChild = BSTF.Node.RightChild;
-        node.Parent = BSTF.Node.Parent;
-        node.RightChild.Parent = node;
-        node.LeftChild.Parent = node;
+        if (BSTF.Node.RightChild.LeftChild != null) {
+//            node.Parent = BSTF.Node.Parent;
+//            node.RightChild = BSTF.Node.RightChild;
+//            node.LeftChild = BSTF.Node.LeftChild;
+//
+//            node.RightChild.Parent = node;
+//            node.LeftChild.Parent = node;
+
+            node.Parent.LeftChild = null;
+            node.LeftChild = BSTF.Node.LeftChild;
+            node.RightChild = BSTF.Node.RightChild;
+            node.Parent = BSTF.Node.Parent;
+
+            if (node.RightChild != null) {
+                node.RightChild.Parent = node;
+            }
+
+            if (node.LeftChild != null) {
+                node.LeftChild.Parent = node;
+            }
+        } else {
+            node.Parent = BSTF.Node.Parent;
+
+            node.LeftChild = BSTF.Node.LeftChild;
+            node.LeftChild.Parent = node;
+        }
     }
 
     public BSTNode<T> DeleteThirdRec(BSTNode<T> Node) {
@@ -208,47 +260,4 @@ class BST<T> {
 
         return count;
     }
-}
-
-class Test {
-    public static void main(String[] args) {
-        BSTNode<Integer> root = new BSTNode<>(8, 0, null);
-        BST<Integer> tree = new BST<>(root);
-
-//        for (int i = 1; i < 16; i++) {
-//            tree.AddKeyValue(i , i);
-//        }
-
-        System.out.println("123");
-
-
-//        System.out.println(tree.AddKeyValue(, 0));
-//        System.out.println(tree.AddKeyValue(2, 0));
-
-//        BSTNode<Integer> testNode = tree.FinMinMax(root, true);
-
-        tree.AddKeyValue(4 , 4);
-        tree.AddKeyValue(12 , 4);
-        tree.AddKeyValue(2 , 4);
-        tree.AddKeyValue(6 , 4);
-        tree.AddKeyValue(10 , 4);
-        tree.AddKeyValue(14 , 4);
-        tree.AddKeyValue(1 , 4);
-        tree.AddKeyValue(3 , 4);
-        tree.AddKeyValue(5 , 4);
-        tree.AddKeyValue(7 , 4);
-        tree.AddKeyValue(9 , 4);
-        tree.AddKeyValue(11 , 4);
-        tree.AddKeyValue(13 , 4);
-        tree.AddKeyValue(15 , 4);
-
-        System.out.println(tree.DeleteNodeByKey(12));
-
-
-
-//        System.out.println(tree.DeleteNodeByKey(2));
-
-
-    }
-
 }
